5e2dfcea578d5cd3f086ab7c2608e9cbb3039ed5
Estera Dell <zelasko875@gmail.com>
2024-10-03 10:49:32 +0200

    notatki

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <LPC213X.H>
+void DAC_MCP4921_Set(unsigned int uiVoltage);
+
+#define P04_SCK0 			(1<<8)
+#define P05_SDO	 			(1<<10)	//MIS0
+#define P06_SDI	 			(1<<12)	//M0SI
+#define P010_CS  			(1<<10)				//Chip select
+#define CONFIG_BITS  	0xc00
+#define VREF 					5
+#define SPIF 					(1<<7)
+
+
+
+int  main(){
+	DAC_MCP4921_Set(2);
+	while(1){}
+}
+//00101010
+
+void DAC_MCP4921_Set(unsigned int uiVoltage) {
+	
+	//unsigned char data_to_send;
+	volatile unsigned char status;
+	volatile unsigned char dummy ;
+	uiVoltage =  (uiVoltage << 12)/5;
+	
+	PINSEL0 = PINSEL0 | P04_SCK0 | P05_SDO | P06_SDI;
+
+	IODIR0 = IODIR0 | P010_CS;
+	
+
+	S0SPCR |= 0x0030;
+	S0SPCCR = 0x08;
+	
+	IOCLR0 = IOCLR0 |  P010_CS;
+	
+	
+	//data_to_send = (CONFIG_BITS | ((uiVoltage >> 8) & 0x0F));
+	S0SPDR = 0xcfff;
+	
+	
+	while(!(S0SPSR & SPIF)){} 
+	status = S0SPSR;
+	dummy = S0SPDR;
+	//data_to_send = uiVoltage & 0xFF;
+	S0SPDR =0xf0ff;
+		
+	
+	while(!(S0SPSR & SPIF)){} 
+	status = S0SPSR;
+	dummy = S0SPDR;
+	
+	IOSET0 |= P010_CS;
+}
+
